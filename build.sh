6cc6055f0910773ff49d6b1bad3057e357664b08
stooged <stooged@users.noreply.github.com>
2018-06-24 12:48:39 +0800

    add win make

--- build.sh
+++ build.sh
@@ -6,8 +6,10 @@ popd
 pushd Enable_VR
 make
 popd
+mkdir -p bin
 rm -f bin/Enable_VR.bin
 cp Enable_VR/Enable_VR.bin bin/Enable_VR.bin
+mkdir -p html_payload
 tool/bin2js bin/Enable_VR.bin > html_payload/payload.js
 sed "s/###/$(cat html_payload/payload.js)/" exploit.template > html_payload/Enable_VR.html
 rm -f Enable_VR/Enable_VR.bin
