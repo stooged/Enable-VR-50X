d7cd91ee347fe655bb2e6fadac588b7e1ee34072
stooged <stooged@users.noreply.github.com>
2018-06-29 15:08:35 +0800

    cleanup

--- Enable_VR/source/main.c
+++ Enable_VR/source/main.c
@@ -1,12 +1,6 @@
 #include "ps4.h"
 #include "patch.h"
 
-void systemMessage(char* msg) {
- 	char buffer[512]; 
- 	sprintf(buffer, "%s\n\n\n\n\n\n\n", msg);
- 	sceSysUtilSendSystemNotificationWithText(0x81, buffer);
-}
-
 int _main(struct thread *td) {
 	initKernel();
 	initLibc();
