de84b4aac2e50a009364c95e0002482ee719b35c
stooged <stooged@users.noreply.github.com>
2018-07-01 12:51:44 +0800

    Initial

--- build.sh
+++ build.sh
@@ -11,6 +11,13 @@ rm -f bin/Enable_VR.bin
 cp Enable_VR/Enable_VR.bin bin/Enable_VR.bin
 mkdir -p html_payload
 tool/bin2js bin/Enable_VR.bin > html_payload/payload.js
-sed "s/###/$(cat html_payload/payload.js)/" exploit.template > html_payload/Enable_VR.html
+FILESIZE=$(stat -c%s "bin/Enable_VR.bin")
+PNAME=$"Enable VR"
+cp exploit.template html_payload/Enable_VR.html
+sed -i -f - html_payload/Enable_VR.html << EOF
+s/#NAME#/$PNAME/g
+s/#BUF#/$FILESIZE/g
+s/#PAY#/$(cat html_payload/payload.js)/g
+EOF
 rm -f Enable_VR/Enable_VR.bin
 rm -f html_payload/payload.js
